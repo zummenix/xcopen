5299e95a6dd47a23e6395e43767370e5c7acdb92
Aleksey Kuznetsov <zummenix@gmail.com>
2018-05-30 21:10:07 +0500

    Implement some prototype stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate walkdir;
+
+use std::env;
+use walkdir::{DirEntry, WalkDir};
+
 fn main() {
-    println!("Hello, world!");
+    let current_dir = env::current_dir().expect("an access to a current directory");
+    println!("{:?}", current_dir);
+    for entry in WalkDir::new(current_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(is_xcode_file)
+    {
+        println!("{}", entry.path().display());
+    }
 }
+
+fn is_xcode_file(entry: &DirEntry) -> bool {
+    if let Some(ext) = entry.path().extension() {
+        ext == "xcodeproj" || ext == "xcworkspace"
+    } else {
+        false
+    }
+}
+
+// TODO: handle paths under .xcodeproj:
+// /Users/zummenix/projects/Backgrounder/Backgrounder.xcodeproj
+// /Users/zummenix/projects/Backgrounder/Backgrounder.xcodeproj/project.xcworkspace
+// /Users/zummenix/projects/Backgrounder/Pods/Pods.xcodeproj
+// /Users/zummenix/projects/Backgrounder/Backgrounder.xcworkspace
+
+// TODO: filter Pods (smart)
+// /Users/zummenix/projects/Backgrounder/Backgrounder.xcodeproj
+// /Users/zummenix/projects/Backgrounder/Pods/Pods.xcodeproj
+// /Users/zummenix/projects/Backgrounder/Backgrounder.xcworkspace
+
+// TODO: filter note_modules (smart)
+// /Users/zummenix/projects/RNApp/node_modules/react-native/Libraries/Sample/Sample.xcodeproj
+// /Users/zummenix/projects/RNApp/node_modules/react-native/Libraries/LinkingIOS/RCTLinking.xcodeproj
