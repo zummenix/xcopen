1fc4e799a3a3816f675802726ba845a02f0f680d
Aleksey Kuznetsov <zummenix@gmail.com>
2018-05-31 08:59:08 +0500

    Sketch some draft interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate walkdir;
 
+use std::path::{Path, PathBuf};
 use walkdir::DirEntry;
 
 pub fn is_xcode_file(entry: &DirEntry) -> bool {
@@ -10,6 +11,35 @@ pub fn is_xcode_file(entry: &DirEntry) -> bool {
     }
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum FileKind {
+    Project,
+    Workspace,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct FileModel {
+    pub path: PathBuf,
+    pub kind: FileKind,
+}
+
+pub fn files(root: &Path) -> Vec<FileModel> {
+    Vec::new()
+}
+
+pub trait FilePathProvider {
+    fn iter(&self) -> Box<Iterator<Item = &FilePath>>;
+}
+
+pub trait FilePath {
+    fn path(&self) -> &Path;
+}
+
+impl FilePath for DirEntry {
+    fn path(&self) -> &Path {
+        self.path()
+    }
+}
 // TODO: handle paths under .xcodeproj:
 // /Users/zummenix/projects/Backgrounder/Backgrounder.xcodeproj
 // /Users/zummenix/projects/Backgrounder/Backgrounder.xcodeproj/project.xcworkspace
