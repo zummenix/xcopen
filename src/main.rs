72dba60facb7eaae0063e8c64b0b6376465440db
Aleksey Kuznetsov <zummenix@gmail.com>
2018-06-03 15:00:16 +0500

    Implement base to open projects

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 extern crate xcopen;
 
 use std::env;
-use xcopen::files;
+use std::path::{Path, PathBuf};
 
 fn main() {
     let root = env::current_dir().expect("an access to a current directory");
-    println!("{:?}", root);
-
-    for file in files(&root) {
-        println!("{:?}", file);
+    let files = xcopen::files(&root);
+    if files.len() == 0 {
+        println!("No projects found");
+    } else if files.len() == 1 {
+        open(&files[0]);
+    } else if files.len() == 2 {
+        let first = &files[0];
+        let second = &files[1];
+        if xcopen::is_xcodeproj(&first) && xcopen::is_xcworkspace(&second) {
+            open(&second);
+        } else if xcopen::is_xcodeproj(&second) && xcopen::is_xcworkspace(&first) {
+            open(&first);
+        } else {
+            show(&files);
+        }
+    } else {
+        show(&files);
     }
+}
+
+fn open(path: &Path) {
+    use std::process::Command;
+    Command::new("open")
+        .arg(path)
+        .output()
+        .expect("open a project");
+}
+
+fn show(paths: &[PathBuf]) {
+    println!("{:?}", paths);
 }
