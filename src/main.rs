b3f7945f92ebb4915fbc3bf16bbfa6f5754ef79d
Aleksey Kuznetsov <zummenix@gmail.com>
2018-07-18 09:00:24 +0500

    Use locked stdout for output

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,34 @@ use xcopen::Decision;
 
 use std::collections::HashMap;
 use std::env;
+use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 
 fn main() {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
     let root = env::current_dir().expect("an access to a current directory");
     match xcopen::decision(&root) {
         Decision::NoEntries => {
-            println!("No xcworkspace/xcodeproj file found under current directory");
+            writeln!(
+                &mut handle,
+                "No xcworkspace/xcodeproj file found under current directory"
+            );
         }
         Decision::Open(path) => open(&path),
         Decision::Show(groups) => {
             let mut number: u32 = 1;
             let mut map: HashMap<u32, PathBuf> = HashMap::new();
             for (group, projects) in groups {
-                println!("in {}:", group.to_string_lossy());
+                writeln!(&mut handle, "in {}:", group.to_string_lossy());
                 for project in projects {
                     if let Some(file_name) = project.file_name() {
-                        println!("   {}. {}", number, file_name.to_string_lossy());
+                        writeln!(
+                            &mut handle,
+                            "   {}. {}",
+                            number,
+                            file_name.to_string_lossy()
+                        );
                         map.insert(number, project.to_owned());
                         number += 1;
                     }
